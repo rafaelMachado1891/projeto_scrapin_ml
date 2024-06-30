6848717333d9b685230342a831834e2c23475ff1
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-30 13:47:58 -0300

    atualização do script de scrapin

--- src/ml.py
+++ src/ml.py
@@ -1,14 +1,15 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
+import pandas as pd
+
 
 keyword = "Abajur"
 
-url = "https://lista.mercadolivre.com.br/{keyword}"
+url = f"https://lista.mercadolivre.com.br/{keyword}"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    print(response.json())
+    print(response.text)
 else:
     print("não foi possivel conectar ao site")
