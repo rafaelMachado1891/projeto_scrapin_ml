47c0087aa369d9d4592555e2a765a94caaa5c2ae
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-30 18:06:12 -0300

    atualizacao do codigo

--- src/ml.py
+++ src/ml.py
@@ -11,5 +11,24 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print(response.text)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    search_result = soup.find_all('div', class_='ui-search-result')
+
+    data = []
+
+    for result in search_result:
+        link = result.find('a', class_='ui-search-link')
+        title = result.find('h2', class_='ui-search-item__title').text.strip()
+        price = result.find('span', class_='andes_money_amount__fraction')
+        brand = result.find('span', class_='ui-search-item_title')
+
+        data.append({'Title':title, 'Price':price, 'Brand':brand, 'Link': link})
+
+        if link:
+            link = link.get('href')
+
+        print(data)
 else:
     print("não foi possivel conectar ao site")
